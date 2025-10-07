9a3c49b432e63355dc21070865adebce8ed8a194
Riccardo Tornesello <riccardo.tornesello@gmail.com>
2025-10-07 13:51:05 +0000

    added example offloaded-testbench

---
+++ offloaded-testbench/setup.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+set -e
+
+here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+# shellcheck source=/dev/null
+source "$here/../../common.sh"
+source "$here/../utils.sh"
+
+CLUSTER_NAME_1=rome
+CLUSTER_NAME_2=milan
+
+KUBECONFIG_1=liqo_kubeconf_rome
+KUBECONFIG_2=liqo_kubeconf_milan
+
+MANIFEST_1="$here/manifests/resources_rome.yaml"
+MANIFEST_2="$here/manifests/resources_milan.yaml"
+
+MANIFEST_CALICO_1=https://raw.githubusercontent.com/projectcalico/calico/v3.30.3/manifests/operator-crds.yaml
+MANIFEST_CALICO_2=https://raw.githubusercontent.com/projectcalico/calico/v3.30.3/manifests/tigera-operator.yaml
+MANIFEST_CALICO_3="$here/manifests/calico.yaml"
+
+LIQO_CLUSTER_CONFIG_YAML="$here/manifests/cluster.yaml"
+
+# LIQO_REPO="https://github.com/liqotech/liqo"
+# LIQO_VERSION="v1.0.1"
+
+LIQO_REPO="https://github.com/Wekkser/liqo.git"
+LIQO_VERSION="e300636af16a6ecd02aa661eb5c492243497f93a"
+
+# LIQO_REPO="https://github.com/riccardotornesello/liqo.git"
+# LIQO_VERSION="adc674974e54b9bb80d571db87a2eda380d4d8d6"
+
+check_requirements
+
+delete_all_kind_clusters
+
+create_kind_cluster_no_wait "$CLUSTER_NAME_1" "$KUBECONFIG_1" "$LIQO_CLUSTER_CONFIG_YAML"
+create_kind_cluster_no_wait "$CLUSTER_NAME_2" "$KUBECONFIG_2" "$LIQO_CLUSTER_CONFIG_YAML"
+
+create_resources "$KUBECONFIG_1" "$MANIFEST_CALICO_1"
+create_resources "$KUBECONFIG_1" "$MANIFEST_CALICO_2"
+create_resources "$KUBECONFIG_1" "$MANIFEST_CALICO_3"
+
+create_resources "$KUBECONFIG_2" "$MANIFEST_CALICO_1"
+create_resources "$KUBECONFIG_2" "$MANIFEST_CALICO_2"
+create_resources "$KUBECONFIG_2" "$MANIFEST_CALICO_3"
+
+wait_for_nodes_ready "$KUBECONFIG_1"
+install_liqo_version "$CLUSTER_NAME_1" "$KUBECONFIG_1" "$LIQO_VERSION" "$LIQO_REPO"
+
+wait_for_nodes_ready "$KUBECONFIG_2"
+install_liqo_version "$CLUSTER_NAME_2" "$KUBECONFIG_2" "$LIQO_VERSION" "$LIQO_REPO"
+
+peer_clusters "$KUBECONFIG_1" "$KUBECONFIG_2"
+
+create_namespace "$KUBECONFIG_1" rome-offloaded
+create_namespace "$KUBECONFIG_1" rome-local
+create_namespace "$KUBECONFIG_2" milan-local
+
+offload_namespace "$KUBECONFIG_1" rome-offloaded
+
+apply_resources "$KUBECONFIG_1" "$MANIFEST_1"
+apply_resources "$KUBECONFIG_2" "$MANIFEST_2"
--- utils.sh
+++ utils.sh
@@ -47,3 +47,83 @@ function create_kind_cluster_no_wait() {
         --kubeconfig $kubeconfig --config $config" "Failed to create cluster \"$name\""
     success_clear_line "Cluster \"$name\" has been created."
 }
+
+function peer_clusters() {
+    local kubeconfig="$1"
+    local remote_kubeconfig="$2"
+
+    info "Peering clusters..."
+
+    fail_on_error "liqoctl peer \
+        --kubeconfig $kubeconfig \
+        --remote-kubeconfig $remote_kubeconfig \
+        --gw-server-service-type NodePort" "Failed to peer clusters"
+
+    success_clear_line "Clusters have been peered."
+}
+
+function create_namespace() {
+    local kubeconfig="$1"
+    local name="$2"
+
+    info "Creating namespace \"$name\"..."
+
+    fail_on_error "kubectl create namespace $name --kubeconfig $kubeconfig" "Failed to create namespace \"$name\""
+
+    success_clear_line "Namespace \"$name\" created successfully."
+}
+
+function offload_namespace() {
+    local kubeconfig="$1"
+    local name="$2"
+
+    info "Offloading namespace \"$name\"..."
+
+    fail_on_error "liqoctl offload namespace $name --kubeconfig $kubeconfig" "Failed to offload namespace \"$name\""
+
+    success_clear_line "Namespace \"$name\" offloaded successfully."
+}
+
+function install_liqo_version() {
+    local cluster_name="$1"
+    local kubeconfig="$2"
+    local commit_sha="$3"
+    local repo_url="$4"
+
+    info "Installing liqo on cluster \"$cluster_name\"..."
+
+    shift 4
+    labels="$*"
+
+    fail_on_error "liqoctl install kind --cluster-id $cluster_name \
+        --cluster-labels=$(join_by , "${labels[@]}") \
+        --kubeconfig $kubeconfig \
+        --version $commit_sha \
+        --repo-url $repo_url" "Failed to install liqo on cluster \"$cluster_name\""
+
+    success_clear_line "Liqo has been installed on cluster \"$cluster_name\"."
+}
+
+function wait_for_nodes_ready() {
+    local kubeconfig="$1"
+    local timeout="${2:-300}"  # Default 5 minutes timeout
+    local interval="${3:-5}"   # Default 5 seconds interval
+
+    info "Waiting for all nodes to be ready..."
+
+    local elapsed=0
+    while [ $elapsed -lt $timeout ]; do
+        local not_ready_nodes=$(kubectl get nodes --kubeconfig "$kubeconfig" --no-headers 2>/dev/null | grep -v " Ready " | wc -l)
+
+        if [ "$not_ready_nodes" -eq 0 ]; then
+            success_clear_line "All nodes are ready."
+            return 0
+        fi
+
+        sleep $interval
+        elapsed=$((elapsed + interval))
+    done
+
+    error "Timeout waiting for nodes to be ready after ${timeout}s"
+    return 1
+}
